8ab5b23ed5e2adbd96d4387ae92a90d72352cb61
guilherme <gui.rzamb@gmail.com>
2025-09-28 14:50:52 -0300

    add função deletar aluno

--- crud.py
+++ crud.py
@@ -9,6 +9,8 @@ def criar_aluno(nome, idade):
             conexao.commit()
         except Exception as erro:
             print(f"Erro ao criar aluno: {erro}")
+        else:
+            print("Aluno criado com sucesso!")
         finally:
             cursor.close()
             conexao.close()
@@ -34,6 +36,8 @@ def atualizar_idade(id_aluno, nova_idade):
             
         except Exception as erro:
             print(f"Erro ao tentar atualizar idade: {erro}")
+        else:
+            print("Idade do aluno alterada com sucesso!")
         finally:
             cursor.close()
             conexao.close()
@@ -41,6 +45,25 @@ lista = listar_alunos()
 
 for aluno in lista:
     print(f"Idade - {aluno[0]} - Nome: {aluno[1]} - Idade: {aluno[2]}")
-id = int(input("Digite o id do aluno que você deseja alterar a idade: "))
-idade = int(input("Digite a nova idade do aluno: "))
-atualizar_idade(id, idade)
+# id = int(input("Digite o id do aluno que você deseja alterar a idade: "))
+# idade = int(input("Digite a nova idade do aluno: "))
+# atualizar_idade(id, idade)
+
+
+def deletar_aluno(id):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute("DELETE FROM alunos WHERE id = %s",
+                (id,))
+            conexao.commit()
+            
+        except Exception as erro:
+            print(f"Erro ao tentar atualizar idade: {erro}")
+        else:
+            print("Aluno deletado com sucesso!")
+        finally:
+            cursor.close()
+            conexao.close()
+id = int(input("Digite o id do aluno que você deseja deletar: "))
+deletar_aluno(id)
